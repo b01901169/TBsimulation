90f97416155fe1f40eec32626b0caa59506f1249
b01901169 <b01901169@ntu.edu.tw>
2018-08-28 03:53:52 -0700

    0828 0400

--- src/full/exp_script.sh
+++ src/full/exp_script.sh
@@ -1,1 +1,1 @@
-python exp.py -n 0828J3 -a 1 -b 0.5 -s 5 -J 3 -iteration 400 -count 10
+python exp.py -n 0828J3 -a 1 -b 0.5 -s 5 -J 3 -iteration 200 -count 10
--- src/full/flu_script.sh
+++ src/full/flu_script.sh
@@ -1,1 +1,1 @@
-python flu.py -n 0828_0300_old -a 0.2 -b 3 -s 5 -iteration 300 -count 10
+python flu.py -n 0828_0300_old -a 0.2 -b 2 -s 5 -iteration 300 -count 10
